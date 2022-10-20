83837842b188ef268eef4bd9fe68dd6d8758a18f
xjw <671173782@qq.com>
2022-10-20 18:58:54 +0800

    new file:   .eslintcache 	modified:   .eslintrc.js 	new file:   mock/user.ts 	modified:   src/store/index.ts 	new file:   src/views/homePage/index.vue 	modified:   src/views/login/index.vue 	modified:   types/components.d.ts

---
+++ src/views/homePage/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>这里是主页</div>
+  </div>
+</template>
+
+<script setup name="HomePageIndex">
+  // const { proxy } = getCurrentInstance()
+  // const router = useRouter()
+  // const route = useRoute()
+</script>
+
+<style lang="less" scoped></style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <div>这里是内容</div>
+    <button @click="testAixos">测试</button>
     <!-- Your content -->
   </div>
 </template>
 
 <script setup name="LoginIndex">
-  // const { proxy } = getCurrentInstance()
-  // const router = useRouter()
-  // const route = useRoute()
+  import { useUserStore } from '/@/store';
+  const userStore = useUserStore();
+  const testAixos = async () => {
+    userStore.info();
+    console.log(userStore);
+  };
 </script>
 
 <style lang="less" scoped></style>
